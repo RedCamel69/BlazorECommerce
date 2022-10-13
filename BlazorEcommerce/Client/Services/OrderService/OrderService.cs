8c7f1a9b3b20f8c7b86859cac959673428d1905b
Richard Gilbert <richardjgilbert@hotmail.com>
2022-10-13 15:37:55 +0100

    client order creation

--- BlazorEcommerce/Client/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/OrderService.cs
@@ -1,6 +1,39 @@
-﻿namespace BlazorEcommerce.Client.Services.OrderService
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Services.OrderService
 {
-    public class OrderService :IOrderService
+    public class OrderService : IOrderService
     {
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+        public OrderService(HttpClient httpClient,
+            AuthenticationStateProvider authStateProvider,
+            NavigationManager navigationManager)
+        {
+            _http = httpClient;
+            _authenticationStateProvider = authStateProvider;
+            _navigationManager = navigationManager;
+        }
+
+        public AuthenticationStateProvider AuthStateProvider { get; }
+
+        public async Task PlaceOrder()
+        {
+           if (await IsUserAuthenticated())
+            {
+                await _http.PostAsync("api/order", null);
+            }
+            else
+            {
+                _navigationManager.NavigateTo("login");
+            }
+        }
+
+        private async Task<bool> IsUserAuthenticated()
+        {
+            return (await _authenticationStateProvider.GetAuthenticationStateAsync()).User.Identity.IsAuthenticated;
+        }
     }
 }
