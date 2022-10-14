89df687b15a0bee5b3e02b38f37953f50524732f
Richard Gilbert <richardjgilbert@hotmail.com>
2022-10-14 12:24:55 +0100

    clear cart after placing order

--- BlazorEcommerce/Server/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Server/Services/OrderService/OrderService.cs
@@ -48,7 +48,14 @@ namespace BlazorEcommerce.Server.Services.OrderService
 
             _context.Orders.Add(order);
 
+            _context.CartItems.RemoveRange(
+                _context.CartItems.Where(x => x.UserId == GetUserId()));
+
+
             await _context.SaveChangesAsync();
+
+
+
 
             return new ServiceResponse<bool> { Data = true };
         }
