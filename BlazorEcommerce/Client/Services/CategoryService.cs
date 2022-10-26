4af1cf6083eed12a047bddcbb793146fc6c17584
Richard Gilbert <RichardJGilbert@hotmail.com>
2022-10-26 12:58:19 +0100

    client admin page for categories

--- BlazorEcommerce/Client/Services/CategoryService.cs
+++ BlazorEcommerce/Client/Services/CategoryService.cs
@@ -1,12 +1,17 @@
-﻿namespace BlazorEcommerce.Client.Services
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http;
+
+namespace BlazorEcommerce.Client.Services
 {
     public class CategoryService : ICategoryService
     {
 
         private readonly HttpClient _http;
 
-        public List<Category> Categories { get; set; }
+        public event Action OnChange;
 
+        public List<Category> Categories { get; set; }
+        public List<Category> AdminCategories { get; set;}
 
         public CategoryService(HttpClient httpClient)
         {
@@ -22,6 +27,48 @@
                 Categories = res.Data;
         }
 
+        public async Task GetAdminCategories()
+        {
+            var res =
+ await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category/admin");
 
+            if (res != null && res.Data != null)
+                AdminCategories = res.Data;
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            
+             var result=    await _http.PostAsJsonAsync("api/category/admin", category);
+            AdminCategories = result.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>().Result.Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public async Task UpdateCategory(Category category)
+        {
+            var response = await _http.PutAsJsonAsync("api/Category/admin", category);
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var response = await _http.DeleteAsync($"api/Category/admin/{categoryId}");
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public Category CreateNewCategory()
+        {
+            var newCategory = new Category { IsNew = true, Editing = true }; //strange bug fix
+            AdminCategories.Add(newCategory);
+            OnChange.Invoke();
+            return newCategory;
+        }
     }
 }
