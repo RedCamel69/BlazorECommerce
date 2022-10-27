cde5caefe9ff559b2b531cd5a187f5ee6ad83c40
Richard Gilbert <RichardJGilbert@hotmail.com>
2022-10-27 16:35:03 +0100

    Admin Product Types

---
+++ BlazorEcommerce/Server/Controllers/ProductTypeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize(Roles="Admin")]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IProductTypeService _productTypeService;
+
+        public ProductTypeController(IProductTypeService productTypeService)
+        {
+            _productTypeService = productTypeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> GetProductTypes()
+        {
+            var result = await _productTypeService.GetProductTypes();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> AddProductType(ProductType productType)
+        {
+            var result = await _productTypeService.AddProductType(productType);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<ProductType>>>> UpdateProductType(ProductType productType)
+        {
+            var result = await _productTypeService.UpdateProductType(productType);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> ProductType(int id)
+        {
+            var result = await _productTypeService.DeleteProductType(id);
+            return Ok(result);
+        }
+    }
+}
